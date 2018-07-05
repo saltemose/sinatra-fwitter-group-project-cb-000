9976e50f6c6aa6632a6164df9f668b565b461be4
Sean Altemose <sean@seanaltemose.com>
2018-07-05 00:26:15 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   get '/tweets' do
     if logged_in?
       @tweets = Tweet.all
-      erb :'/tweets/tweets'
+      erb :index
     else
       flash[:message] = "You must be logged in to view tweets."
       redirect :'/login'
