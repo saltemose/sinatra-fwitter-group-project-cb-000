6d85f965d720414f4177be19f7738ceb73030de4
Sean Altemose <sean@seanaltemose.com>
2018-07-05 00:06:07 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
       @user = User.find_by(username: params[:username])
       puts @user.to_json
       puts params[:password]
+      puts @user
       if @user && @user.password_digest == params[:password]
         puts @user.id
         session[:user_id] = @user.id
