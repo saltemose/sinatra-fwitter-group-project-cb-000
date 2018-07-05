fc0f51aa972561a4398d198cc0780e6e30b80874
Sean Altemose <sean@seanaltemose.com>
2018-07-05 02:12:02 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -47,7 +47,6 @@ class TweetsController < ApplicationController
   get '/tweets/:id/edit' do
     if logged_in?
       @tweet = Tweet.find(params[:id])
-      puts @tweet.to_json
       if @tweet && session[:user_id] == @tweet.user_id
         erb :'/tweets/edit_tweet'
       else
