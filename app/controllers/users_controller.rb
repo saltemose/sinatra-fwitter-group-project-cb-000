a3e48b564715ae1cffe21f43ed0909bdeda736f1
Sean Altemose <sean@seanaltemose.com>
2018-07-05 00:03:15 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,14 +18,13 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    puts params
-
     if params[:username].empty? || params[:password].empty?
       puts params
       flash[:message] = "Username and password are required to sign in. Try again."
       erb :'/users/login'
     else
       @user = User.find_by(username: params[:username])
+      puts @user.to_json
       if @user && @user.password == params[:password]
         puts @user.id
         session[:user_id] = @user.id
